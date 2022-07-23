7eca543e2f38e79cc7794165db367f9842db3c5e
Raniere Silva <raniere@rgaiacs.com>
2022-07-23 16:51:12 +0800

    Add card to UI

---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+
+shinyUI(fluidPage(
+    tags$head(
+        tags$link(rel = "stylesheet", type = "text/css", href = "poker.css")
+    ),
+
+    titlePanel("Poker"),
+
+    fluidRow(
+        column(12,
+               div(
+                   class="card"
+               )
+        )
+    )
+))
